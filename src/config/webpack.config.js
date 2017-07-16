97ac690bdf440772608e74c15ff7302c7ce5892b
Jess Lin <perdendosi@gmail.com>
2017-07-16 15:32:08 -0700

    Add webpack dependency

---
+++ src/config/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const moduleRoot = path.resolve(__dirname, '../../');
+
+
+module.exports = {
+  // The absolute path to for resolving entry points and loaders from this config.
+  context: moduleRoot,
+
+  entry: {
+    'init': './src/js/init.js',
+    'main': './src/js/main.js',
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(moduleRoot, 'output'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(moduleRoot, 'src', 'js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Avoid needing to run the potentially expensive Babel recompilation process on each run.
+            // Since we only specify `true`, it will use the default dir node_modules/.cache/babel-loader.
+            cacheDirectory: true,
+          }
+        }
+      },
+
+    ],
+  },
+  devtool: 'cheap-module-eval-source-map',
+};
