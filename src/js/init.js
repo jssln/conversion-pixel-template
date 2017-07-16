81c8b7e4ce599d5b0d852da6171ffe12922c7199
Jess Lin <perdendosi@gmail.com>
2017-07-16 08:37:57 -0700

    Add babel + flow deps, and init.js

---
+++ src/js/init.js
@@ -0,0 +1,11 @@
+// @flow
+
+/**
+ * This code initializes the conversion pixel. It has 2 jobs:
+ *   1. Download the "main" code -- the actual core logic of the pixel.
+ *   2. Create a queue to keep track of any events that are fired before the main code is downloaded.
+ */
+
+function() {
+
+}
